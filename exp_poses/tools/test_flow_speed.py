41b12781aab9921a547f7d6370e8596fcb208964
TWJianNuo <twjiannuo@gmail.com>
2021-05-23 09:25:20 -0400

    add speed test

--- exp_poses/tools/test_flow_speed.py
+++ exp_poses/tools/test_flow_speed.py
@@ -46,7 +46,7 @@ def read_splits(args):
     evaluation_entries = [x.rstrip('\n') for x in open(os.path.join(split_root, 'test_files.txt'), 'r')]
     odom_entries = get_odomentries(args)
 
-    return odom_entries + train_entries + evaluation_entries
+    return train_entries + evaluation_entries
 
 def remove_dup(entries):
     dupentry = list()
